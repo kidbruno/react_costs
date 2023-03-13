23075e67ed07a9a6b69aa0d4156695f341dde2f3
Bruno <31630665+kidbruno@users.noreply.github.com>
2023-03-12 21:24:26 -0300

    criando form

---
+++ src/components/project/ProjectForm.jsx
@@ -0,0 +1,38 @@
+import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+
+function ProjectForm({ btnText }){
+    
+    return (
+
+        <form className={styles.form}>
+            <Input 
+                type={"text"}
+                text={"Nome do Projeto"}
+                name={"name"} 
+                placeholder={"Insira o nome do Projeto"}
+
+            />
+           <Input 
+                type={"number"}
+                text={"Orçamento total do Projeto"}
+                name={"budget"} 
+                placeholder={"Insira o orçamento total"}
+                
+            />
+            <Select
+                name={"category_id"}
+                text={"Selecione uma categoria"}
+                
+            />
+            <SubmitButton 
+                text={btnText} 
+            />
+            
+        </form>
+    )
+}
+
+export default ProjectForm
