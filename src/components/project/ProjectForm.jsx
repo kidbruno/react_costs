c9a58762c60639024472266577e4e18b719494a0
Bruno <sandrobruno.013@gmail.com>
2023-03-13 11:05:06 -0300

    conf db

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
@@ -5,6 +7,23 @@ import SubmitButton from '../form/SubmitButton'
 
 function ProjectForm({ btnText }){
     
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
 
         <form className={styles.form}>
@@ -25,6 +44,7 @@ function ProjectForm({ btnText }){
             <Select
                 name={"category_id"}
                 text={"Selecione uma categoria"}
+                option={categories}
                 
             />
             <SubmitButton 
