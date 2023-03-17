223da3cd5e3361f6e603bce1cec9602af4b9383d
Bruno <sandrobruno.013@gmail.com>
2023-03-17 15:53:49 -0300

    implementando request

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,8 +1,9 @@
 import { useNavigate } from 'react-router-dom'
 import Message from '../layout/Message'
-import { styles } from './Projects.module.css'
+import styles from './Projects.module.css'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
+import { useEffect } from 'react'
 
 function Projects() {
 
@@ -13,16 +14,26 @@ function Projects() {
         message = location.state.message
     }
 
+    useEffect(() => {
+        fetch('http://localhost5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((resp) => resp.json)
+    })
+
     return(
 
-        <div> 
-            <div>
+        <div className={styles.project_container}> 
+            <div className={styles.title_container}>
                 <h1>Meus Projetos</h1>
                 <LinkButton to="/newproject" text="Criar Projeto"/>
             </div>
             {message && <Message type="success" msg={message}/>}
             <Container customClass="start">
-                <p>Projetos...</p>
+                <p>Projetos...</p>  
             </Container>
         </div>
  
---
+++ src/components/project/ProjectCard.jsx
@@ -0,0 +1,14 @@
+import styles from './ProjectCard.module.css'
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
+
+function ProjectCard({id, name, budget, category, handleRemove}){
+
+    return(
+
+        <div>
+            <p>Projeto</p>
+        </div>
+    )
+}
+
+export default ProjectCard
