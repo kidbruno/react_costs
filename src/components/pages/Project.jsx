b53ce7417631e77df83204ec60afba03f3bc160a
Bruno <31630665+kidbruno@users.noreply.github.com>
2023-03-20 21:21:01 -0300

    implementando edição e detalhes do projeto

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,6 +1,10 @@
 //  import styles from './Project.module.css'
  import { useParams } from 'react-router-dom'
  import { useState, useEffect } from 'react'
+ import Loading from '../layout/Loading'
+ import Container from '../layout/Container'
+ import styles from './Project.module.css'
+ import ProjectForm from '../project/ProjectForm'
 
 function Project(){
     
@@ -8,30 +12,92 @@ function Project(){
     console.log(id)
 
     const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
 
     useEffect(() => {
         
-        fetch(`http://localhost:5000/project/${id}`, {
-            method: 'GET',
+       setTimeout(() => {
+
+            fetch(`http://localhost:5000/projects/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data)
+            })
+            .catch((err) => console.log(err))
+       }, 1000)
+    }, [id])
+
+    function editPost(){
+        //budget validation
+        // if(project.budget < project.cost){
+        // //mensagem
+        // }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
             },
+            body: JSON.stringify(project),
         })
-        .then(resp => resp.json())
+        .then((resp) => resp.json())
         .then((data) => {
+            
             setProject(data)
+            setShowProjectForm(false)
         })
         .catch((err) => console.log(err))
+    }
 
-
-    }, [id])
+    function toggleProjectForm() {
+        setShowProjectForm(!showProjectForm)
+    }
 
     return (
-        <div>
-            <p>Projeto</p>
-        </div>
-        
+        <> 
+            {project.name ? (
+                <div className={styles.project_details}>
+                    <Container customClass="column">
+                        <div className={styles.details_container}>
+                            <h1>Projeto: {project.name}</h1>
+                            <button onClick={toggleProjectForm} className={styles.btn}> 
+                                {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
+                            </button>
 
+                            {!showProjectForm ? 
+                                <div className={styles.project_info}>
+                                    <p>
+                                        <span>Categoria:</span> {project.category.name}
+                                    </p>
+                                    <p>
+                                        <span>Valor Total do Orçamento:</span> R${project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total Utilizado:</span> R${project.cost}
+                                    </p>
+                                </div>
+                             : 
+                                <div className={styles.project_info}>
+                                    <ProjectForm 
+                                        handleSubmit={editPost} 
+                                        btnText="Concluir Edição" 
+                                        projectData={project} 
+                                    />
+                                </div>
+                             }
+
+                        </div>
+                    </Container>
+                </div>
+            ): (
+                <Loading/> 
+            )} 
+        </>
     )
 }
 
