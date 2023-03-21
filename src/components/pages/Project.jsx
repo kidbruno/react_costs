3554561658c90866b4daa57bc448b884a9b6b9d9
Bruno <sandrobruno.013@gmail.com>
2023-03-21 17:31:08 -0300

    event edit

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -5,6 +5,7 @@
  import Container from '../layout/Container'
  import styles from './Project.module.css'
  import ProjectForm from '../project/ProjectForm'
+ import Message from '../layout/Message'
 
 function Project(){
     
@@ -13,6 +14,9 @@ function Project(){
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
+    const [message, SetMessage] = useState()
+    const [type, setType] = useState()
 
     useEffect(() => {
         
@@ -32,11 +36,15 @@ function Project(){
        }, 1000)
     }, [id])
 
-    function editPost(){
+    function editPost(project){
+        SetMessage('')
+
         //budget validation
-        // if(project.budget < project.cost){
-        // //mensagem
-        // }
+        if(project.budget < project.cost){
+            SetMessage('O orçamento não pode ser menor que o custo do projeto!')
+            setType('error')
+            return false
+        }
 
         fetch(`http://localhost:5000/projects/${project.id}`, {
             method: 'PATCH',
@@ -50,6 +58,8 @@ function Project(){
             
             setProject(data)
             setShowProjectForm(false)
+            SetMessage('Projeto atualizado!')
+            setType('success')
         })
         .catch((err) => console.log(err))
     }
@@ -58,18 +68,23 @@ function Project(){
         setShowProjectForm(!showProjectForm)
     }
 
+    function toggleServiceForm(){
+        setShowServiceForm(!showServiceForm)
+    }
+
     return (
         <> 
             {project.name ? (
                 <div className={styles.project_details}>
                     <Container customClass="column">
+                        {message && <Message type={type} msg={message} />}
                         <div className={styles.details_container}>
                             <h1>Projeto: {project.name}</h1>
                             <button onClick={toggleProjectForm} className={styles.btn}> 
                                 {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
                             </button>
 
-                            {!showProjectForm ? 
+                            {!showProjectForm ? (
                                 <div className={styles.project_info}>
                                     <p>
                                         <span>Categoria:</span> {project.category.name}
@@ -81,7 +96,7 @@ function Project(){
                                         <span>Total Utilizado:</span> R${project.cost}
                                     </p>
                                 </div>
-                             : 
+                             ) : (
                                 <div className={styles.project_info}>
                                     <ProjectForm 
                                         handleSubmit={editPost} 
@@ -89,11 +104,19 @@ function Project(){
                                         projectData={project} 
                                     />
                                 </div>
-                             }
-
+                            )}
                         </div>
-                    </Container>
-                </div>
+                        <div className={styles.service_form_container}>
+                             <h2>Adicione um Serviço:</h2>
+                             <button onClick={toggleServiceForm} className={styles.btn}> 
+                                {!showProjectForm ? 'Adicionar Serviço' : 'Fechar'}
+                            </button>
+                        </div>
+                        <div className='styles.project_info'>
+                                {showServiceForm && <div>Formulário do Serviço.</div>}
+                        </div>
+                    </Container>  
+                </div> 
             ): (
                 <Loading/> 
             )} 
